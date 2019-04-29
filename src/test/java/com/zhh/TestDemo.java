4e9d57bd17781be41068fc86aa9eee382d3e50d6
admin <32248133@qq.com>
2019-04-29 18:32:54 +0800

    orcle第一次连接测试

---
+++ src/test/java/com/zhh/TestDemo.java
@@ -0,0 +1,24 @@
+package com.zhh;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TestDemo {
+    @Test
+    public void connectOrcle() throws Exception {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.204.131:1521:orcl", "system", "root");
+        PreparedStatement pst = connection.prepareStatement("SELECT * from person ");
+//        pst.setObject(1,"aaa");
+        ResultSet resultSet = pst.executeQuery();
+
+        while (resultSet.next()){
+            System.out.println(1);
+            System.out.println(resultSet.getObject("pname"));
+        }
+    }
+}
